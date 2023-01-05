43dfbc7f9c3f26e1a481c3e8a7701f0e5986c6d8
Artem Chernyshev <artem.octopus@mail.ru>
2023-01-05 15:49:09 +0300

    Updates

---
+++ static/js/617.570b9af8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweather_app=self.webpackChunkweather_app||[]).push([[617],{617:function(t,e,r){r.d(e,{Hi9:function(){return a}});var n=r(983);function a(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"}}]})(t)}}}]);
+//# sourceMappingURL=617.570b9af8.chunk.js.map
